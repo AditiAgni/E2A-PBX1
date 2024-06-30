8b3ab174b9cee3b95f112ed50282c08d0acd41bf
Aditi Agnihotri <134733256+AditiAgni@users.noreply.github.com>
2024-06-30 20:25:53 +0530

    Update Pre-processing 26281.R

--- Pre-processing 26281.R
+++ Pre-processing 26281.R
@@ -1,47 +1,35 @@
-#set wd- session-choose directory-"C:/Users/agnih/OneDrive/Desktop/dissertation/wd/GSE26281"
+#set wd-"C:/Users/agnih/OneDrive/Desktop/dissertation/wd/GSE26281" or session-choose directory
 
-#cel files unpacking
+#cel files download and unpacking
 gse26281<-list.celfiles("GSE26281/", pattern="CEL")
 
 
 #reading raw data
 RD_gse26281<-ReadAffy(verbose = TRUE, filenames = gse26281)
-
-#visualization of raw data before normalization
-#hist(RD_gse26281)
-#RAWEXP<-exprs(RD_gse26281)
-#ma.plot(rowMeans(log2(RAWEXP)), log2(RAWEXP[,1])-log2(RAWEXP[,2]), cex=1)
-#boxplot(RD_gse26281)
+boxplot(RD_gse26366) #visualization of raw data before normalization
 
 
 #phenodata import and read
-file.exists("GSE26281_pdata.txt")
-PD_gse26281<-read.table("GSE26281_pdata.txt", header = TRUE)
-#alternate- read.delim("GSE26281_pdata.txt", header = TRUE, fill = TRUE)
-#options(max.print = 380)
-#alternate through rio- PD_gse26281<-import("GSE26281_pdata.txt")
-#head(Phdata)
-#for excel file- PD_gse26281<-import("GSE26281_series_matrix.xlsx")
-#head(rio_xlsx)
-
+file.exists("GSE26281_updata.xlsx")
+UPD_gse26281= import("GSE26281_updata.xlsx") #phenodata having only common samples
 #viewing phenodata = View(Phdata), for dimensions= dim(file name)
 
 
-#RMA normalization
-ND_gse26281 = rma(RD_gse26281)
+#filtering samples from expression data; retaining only common samples
+keepSamples <- UPD_gse26281$Sample_IDs
+rownames(RD_gse26281@phenoData@data) <- sub("_.*", "", rownames(RD_gse26281@phenoData@data))
+rownames(RD_gse26281@protocolData@data) <- sub("_.*", "", rownames(RD_gse26281@protocolData@data))
+FtRD_gse26281 <- RD_gse26281[, colnames(RD_gse26281) %in% keepSamples]
 
-#viewing normalized data= View(ND_gse26281) 
-#boxplot(ND_gse26281@assayData$exprs)
+
+#RMA normalization
+ND_gse26281 = rma(FtRD_gse26281)
+boxplot(ND_gse26281@assayData$exprs) #viewing normalized data= View(ND_gse26281)
 
 
 #getting expression value or probe IDs for genes
 ED_gse26281<-as.data.frame(exprs(ND_gse26281))
-
-
-#merging ED and Phdata for allotment of probe IDs to gene IDs
-colnames(ED_gse26281) = PD_gse26281$Sample_IDs
-#to view- colnames(ED_gse26281)
-#dim(ED_gse26281)- 22283 154
+dim(ED_gse26281) #22283 62
 
 
 #gene annotation through biomart hgnc
@@ -52,17 +40,7 @@ PIDs_gse26281= getBM(attributes = c("affy_hg_u133a_2", "hgnc_symbol"),
                      filters = "affy_hg_u133a_2",
                      values = PIDs.gse26281,
                      mart = Martfunction)
-# alternate- if(interactive()){
-#  mart <- useEnsembl(biomart = "ensembl",
-#                    dataset = "hsapiens_gene_ensembl")
-# 
-#  getBM(attributes = c("affy_hg_u133a", "hgnc_symbol", "chromosome_name", "band"),
-#        filters    = "affy_hg_u133a",
-#        values     = "PIDs.gse26281",
-#        mart       = mart)
-#}
-#dim(PIDs_gse26281) [1] 23467     2
-#to view lists- listMarts(),  listDatasets(mart= ensembl), listAttributes(mart = ensembl)
+dim(PIDs_gse26281) #23652     2
 
 
 #removing unassigned probe IDs
@@ -76,28 +54,26 @@ colnames(PIDs_gse26281.df)[1]<-"PIDs"
 
 #merging gene symbols with ED
 GS_gse26281= merge(PIDs_gse26281.df, ED_gse26281.df, by= "PIDs")
-#dim(GS_gse26281)- 22279 85
+dim(GS_gse26281) #22454 64
 
 
 #limma avereps- to avg out duplicate symbols
 AR_gse26281=as.data.frame((limma::avereps(GS_gse26281, GS_gse26281$hgnc_symbol)))
-#dim 14117  85
+dim(AR_gse26281) #14215  64
+
 
 #final dataset - having umique gene symbols as rownames
 AR_gse26281=column_to_rownames(AR_gse26281, "hgnc_symbol")
-#dim(AR_gse26281)- 14117  84
-
-#removing PIDs column
-FD_gse26281=AR_gse26281[-c(1)]
-#dim(FD_gse26281)- 14117  83
-
-#creating matrix file of final dataset
-FD_gse26281.matrix=as.matrix(FD_gse26281)
+dim(AR_gse26281) #14215  63
+FD_gse26281=AR_gse26281[-c(1)] #removing PIDs column
+dim(FD_gse26281) #14215  62
+FD_gse26281.matrix=as.matrix(FD_gse26281) #creating matrix file of final dataset
 class(FD_gse26281.matrix)= "numeric"
+
 
 #to create common gene symbol column for merging data
 MD_gse26281 <- rownames_to_column (FD_gse26281, "gene.symbol")
-#dim(FD_gse26281)- 14117  84
+dim(FD_gse26281) #14215  62
 
 
 #DGE
@@ -114,9 +90,6 @@ fit2 <- eBayes(fit2)
 DGE_gse26281<-topTable(fit2, coef=1, adjust="BH", confint=0.95)
 DGE2_gse26281<-topTable(fit2, n=Inf, adjust="BH", confint=0.95)
 
-#saving the dge file
-write.csv(DGE_gse26281, file = "1DGE_26281.csv")
-write.csv(DGE2_gse26281, file = "1DGE2_26281.csv")
 
 #saving final final for meta-analysis
-saveRDS(MD_gse26281, file="1MD_gse26281")
+saveRDS(MD_gse26281, file="MD_gse26281")
